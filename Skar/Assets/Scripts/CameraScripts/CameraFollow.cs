e76505da790102bfe4dcdb083eb5d72023477231
Sahilpohare <Sahilpohare@gmail.com>
2018-03-21 13:49:25 +0530

    Movement Done

---
+++ Skar/Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform Target;
+	[Range(0,1)]
+	public float speed;
+	public Vector3 Offset;
+	private Vector3 currentpos;
+	// Use this for initialization
+	void Start () {
+		Init();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Follow();
+	}
+
+	void Init(){
+		if(Target == null){
+			Target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+	}
+    
+		void Follow(){
+		transform.position = Vector3.SmoothDamp(transform.position,Target.position + Offset,ref currentpos,speed);
+	}
+}
---
+++ Skar/Assets/Scripts/PlayerScripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    public float Horizontal;
+	public float Vertical;
+	public bool M1;
+	public bool M2;
+	public float mouseX;
+	public float mouseY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Horizontal = Input.GetAxisRaw("Horizontal");
+		Vertical = Input.GetAxisRaw("Vertical");
+		mouseX = Input.GetAxis("Mouse X");
+		mouseY = Input.GetAxis("Mouse Y");
+		M1 = Input.GetMouseButton(0);
+		M2 = Input.GetMouseButton(1);
+	}
+
+}
